fa671997742e370434c072a41abad406954aff69
Nidra Hayes <nidrahayes@gmail.com>
2024-10-15 14:37:27 -0400

    Multiply Function

--- calc.py
+++ calc.py
@@ -1,19 +1,11 @@
-Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import sys
-... 
-... filename, num1, op, num2 = sys.argv
-... num1, num2 = float(num1), float(num2)
-... 
-... if op == '+':
-...     print(num1 + num2)
-... elif op == '-':
-...     print(num1 - num2)
-... elif op == 'x':
-...     print(num1 * num2)
-... elif op == '/':
-...     print(num1 / num2)
-... elif op == '^':
-...     print(num1 ** num2)
-... else:
-...     print(0)
+def add(x,y):
+    pass
+def subtract(x,y):
+    pass
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    pass
+def square(x):
+    pass
+
